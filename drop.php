c82e2256a87448dbac09dd0b1ecc960416c7f952
bystander <aisy1625@gmail.com>
2026-04-09 20:16:16 +0800

    Delete drop.php

--- drop.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include "config.php";
-
-    $id = $_GET['matrixno'];
-
-    mysqli_query($conn, "DELETE FROM registration WHERE matrixno ='$id'");
-
-    header("Location: view.php");
-?>
